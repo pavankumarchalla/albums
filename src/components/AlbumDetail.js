f8a1af1d74b30a3e557483cae97237751ec98413
Pavan Kumar C <pavankumar@railsfactory.org>
2019-03-29 12:03:48 +0530

    image content added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -8,16 +8,22 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image 
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
             style = {styles.thumbnailStyle}
             source={{ uri: props.album.thumbnail_image }} 
           /> 
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
+          <Text style= {styles.headerTextStyle}>{props.album.title}</Text>
           <Text>{props.album.artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style= {styles.imageStyle}
+          source={ {uri: props.album.image} }
+        />
       </CardSection>
     </Card>
   );
@@ -28,9 +34,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18,
+
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
