c87b6983d0177c74b29ca64a712b74914e461b7d
Pavan Kumar C <pavankumar@railsfactory.org>
2019-03-29 13:04:31 +0530

    Button component added and Linking added to open url when user clicked

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Linking } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
+import Button from './Button'
 
 const AlbumDetail = (props) => {
   console.log('test 2',props)
@@ -24,6 +25,11 @@ const AlbumDetail = (props) => {
           style= {styles.imageStyle}
           source={ {uri: props.album.image} }
         />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(props.album.url)}>
+        Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
